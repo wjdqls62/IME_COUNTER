03e9e34d5dc11af529600fed61789544401fdc8a
jeongbeen.son <wjdqls62@naver.com>
2018-11-15 18:55:58 +0900

    * /sdcard/QA/ 경로에 파일이 없을경우 예외처리 및 Toast 추가

--- app/src/main/java/sonjb/phillit/ime_counter/common/TestCaseManager.java
+++ app/src/main/java/sonjb/phillit/ime_counter/common/TestCaseManager.java
@@ -8,11 +8,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
@@ -52,65 +49,88 @@ public class TestCaseManager {
     }
 
     public void initWord(){
-        typingWord.setText(wordList.get(currentNum).getWord());
-        typingHit.setText(wordList.get(currentNum).gethit());
+        if(wordList != null){
+            typingWord.setText(wordList.get(currentNum).getWord());
+            typingHit.setText(wordList.get(currentNum).gethit());
+        }
+
     }
 
     public void nextWord(){
-        currentNum += 1;
-        typingWord.setText(wordList.get(currentNum).getWord());
-        typingHit.setText(wordList.get(currentNum).gethit());
+        if(wordList != null){
+            currentNum += 1;
+            typingWord.setText(wordList.get(currentNum).getWord());
+            typingHit.setText(wordList.get(currentNum).gethit());
+        }
+
     }
     public void prevWord(){
-        currentNum -= 1;
-        typingWord.setText(wordList.get(currentNum).getWord());
-        typingHit.setText(wordList.get(currentNum).gethit());
+        if(wordList != null){
+            currentNum -= 1;
+            typingWord.setText(wordList.get(currentNum).getWord());
+            typingHit.setText(wordList.get(currentNum).gethit());
+        }
+
+    }
+
+    private File getXls(){
+        File[] fileList = new File("/sdcard/QA/").listFiles();
+        if(fileList == null || fileList.length == 0){
+            Toast.makeText(activity, "파일이 존재하지 않습니다", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        if(fileList.length >= 2){
+            Toast.makeText(activity, "파일이 2개이상 존재합니다\n 엑셀의 단어와 타수는 보여지지 않습니다", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        return fileList[0];
     }
 
     public ArrayList<Word_Hit> loadExcelFile(){
         try {
-            if(wordList == null){
-                wordList = new ArrayList<>();
+            File file = getXls();
+            if(file != null){
+                if(wordList == null){
+                    wordList = new ArrayList<>();
+                }
+                testType = PreferenceManager.getDefaultSharedPreferences(activity).getString("test_type", "KSR_Conversation");
+
+                workbook = Workbook.getWorkbook(file);
+                sheet = workbook.getSheet(testType);
+                sRow = sheet.getRows();
+                sColumns = sheet.getColumns();
+
+                for(int i=0; i<sRow; i++){
+
+                    Word_Hit word_hit = new Word_Hit();
+                    String numCell = sheet.getCell(0, i).getContents();
+                    String wordCell = sheet.getCell(1, i).getContents();
+                    String hitCell = sheet.getCell(2, i).getContents();
+
+                    try{
+                        if(Integer.parseInt(numCell) >= 1 && !numCell.equals("") && !numCell.equals("#")){
+                            Log.d(TAG, "Num : " + numCell + " / Word : " + wordCell + " / Hit : " + hitCell);
+                            word_hit.add(numCell, wordCell, hitCell);
+                            wordList.add(word_hit);
+                        }
+                    }catch (NumberFormatException e){
+                        continue;
+                    }
+                }
+            }else{
+                return null;
             }
-            testType = PreferenceManager.getDefaultSharedPreferences(activity).getString("test_type", "KSR_Conversation");
-            Log.d(TAG, "TestType : " + testType);
-            File file = new File("/sdcard/QA/1.xls");
-            workbook = Workbook.getWorkbook(file);
-            sheet = workbook.getSheet(testType);
-            sRow = sheet.getRows();
-            sColumns = sheet.getColumns();
 
-            Log.d(TAG, "ROW : " + sRow + " / Col : " + sColumns);
-
-             for(int i=0; i<sRow; i++){
-
-                 Word_Hit word_hit = new Word_Hit();
-                 String numCell = sheet.getCell(0, i).getContents();
-                 String wordCell = sheet.getCell(1, i).getContents();
-                 String hitCell = sheet.getCell(2, i).getContents();
-
-                 try{
-                     if(Integer.parseInt(numCell) >= 1 && !numCell.equals("") && !numCell.equals("#")){
-                         Log.d(TAG, "Num : " + numCell + " / Word : " + wordCell + " / Hit : " + hitCell);
-                         word_hit.add(numCell, wordCell, hitCell);
-                         wordList.add(word_hit);
-                     }
-                 }catch (NumberFormatException e){
-                     continue;
-                 }
-            }
-
-            //for(int i=0; i<list.size(); i++){
-            //     Log.d(TAG, "Num : " + list.get(i).getNum() + "/ Word : " + list.get(i).getWord() + "/ Hit : " + list.get(i).gethit());
-            //}
         } catch (IOException e) {
             e.printStackTrace();
-            Toast.makeText(activity, "File Not Found Exception", Toast.LENGTH_SHORT).show();
+            Toast.makeText(activity, "파일 또는 디렉토리가 생성되어 있지 않습니다", Toast.LENGTH_SHORT).show();
         } catch (BiffException e) {
             e.printStackTrace();
             Toast.makeText(activity, ".xls 형식의 확장자만 가능합니다", Toast.LENGTH_SHORT).show();
         }finally {
-            workbook.close();
+            if(workbook != null){
+                workbook.close();
+            }
         }
         return wordList;
     }
